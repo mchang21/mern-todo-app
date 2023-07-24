432587ef2787a20a54ce4828826d095107c2f671
Melvin Chang <melvinchang21@gmail.com>
2023-07-24 16:18:18 -0700

    added create todo, edit todo, and todos list component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CreateTodo from "./components/create-todo.component.js";
+import EditTodo  from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component.js";
+
+class App extends Component {
+  render() {
+    return (
+
+      // Router configuration
+      <Router>
+        <Routes>
+          <Route path="/" exact component ={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </Routes>
+        
+        <div className="container">
+          <h2>MERN-Stack Todo App</h2>
+        </div>
+      </Router>
+
+    );
+  }
 }
 
 export default App;
