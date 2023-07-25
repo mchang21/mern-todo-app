184194e17ba7be0ca8274bc85a9592eb7302ef6b
Melvin Chang <melvinchang21@gmail.com>
2023-07-24 17:23:37 -0700

    finished implementation of create todo component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
-import CreateTodo from "./components/create-todo.component.js";
+import CreateTodo from "./components/create-todo.component";
 import EditTodo  from "./components/edit-todo.component";
-import TodosList from "./components/todos-list.component.js";
+import TodosList from "./components/todos-list.component";
+
+import logo from "./logo.svg"
 
 class App extends Component {
   render() {
     return (
 
-      // Router configuration
       <Router>
-        <Routes>
-          <Route path="/" exact component ={TodosList} />
-          <Route path="/edit/:id" component={EditTodo} />
-          <Route path="/create" component={CreateTodo} />
-        </Routes>
-        
         <div className="container">
-          <h2>MERN-Stack Todo App</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" target="_blank">
+              <img src={logo} width="30" height="30" />
+            </a>
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+
+            <Routes>
+              <Route path="/" exact element ={<TodosList/>} />
+              <Route path="/edit/:id" element={<EditTodo/>} />
+              <Route path="/create" element={<CreateTodo/>} />
+            </Routes>
         </div>
+        
       </Router>
 
     );
