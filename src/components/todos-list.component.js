5ef111f58455b530f5cffc0fe1263393f427ce4e
Melvin Chang <melvinchang21@gmail.com>
2023-07-27 13:40:16 -0700

    implemented functionality to display data from database

--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -1,11 +1,57 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-export default class TodosList extends Component {
-    render() {
-        return (
-            <div>
-                <p>Welcome to Todos List Component!</p>
-            </div>
-        )
-    }
-}
+const Todo = (props) => (
+    <tr>
+        <td>{props.todo.todo_description}</td>
+        <td>{props.todo.todo_responsible}</td>
+        <td>{props.todo.todo_priority}</td>
+        <td>
+            <Link to={"/edit/" + props.todo._id}>Edit</Link>
+        </td>
+    </tr>
+);
+
+const TodosList = () => {
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        const fetchTodos = async () => {
+            try {
+                const response = await axios.get('http://localhost:4000/todos/');
+                setTodos(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        
+        fetchTodos();
+    }, []);
+
+    const todoList = () => {
+        return todos.map((currentTodo) => (
+            <Todo todo={currentTodo} key={currentTodo._id} />
+        ));
+    };
+
+    return (
+        <div>
+            <h3>Todos List</h3>
+            <table className="table table-striped" style={{ marginTop: 20 }}>
+                <thead>
+                    <tr>
+                        <th>Description</th>
+                        <th>Responsible</th>
+                        <th>Priority</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>{console.log(todoList())}</tbody>
+                <tbody>{todoList()}</tbody>
+            </table>
+        </div>
+    );
+};
+
+export default TodosList;
