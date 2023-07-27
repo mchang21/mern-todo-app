32310cec570d07c76e677433ab090d22144c86dc
Melvin Chang <melvinchang21@gmail.com>
2023-07-27 13:36:28 -0700

    updated file

--- src/App.js
+++ src/App.js
@@ -1,48 +1,50 @@
-import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import CreateTodo from "./components/create-todo.component";
 import EditTodo from "./components/edit-todo.component";
 import TodosList from "./components/todos-list.component";
 
-import logo from "./logo.svg"
+import logo from "./logo.svg";
 
-class App extends Component {
-  render() {
-    return (
+const App = () => {
+  return (
+    <Router>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" target="_blank" rel="noopener noreferrer">
+            <img src={logo} width="30" height="30" alt="Logo" />
+          </a>
+          <Link to="/" className="navbar-brand">
+            MERN-Stack Todo App
+          </Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">
+                  Todos
+                </Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/create" className="nav-link">
+                  Create Todo
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <br />
 
-      <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" target="_blank">
-              <img src={logo} width="30" height="30" />
-            </a>
-            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
-            <div className="collpase navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">Todos</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create Todo</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <br />
+        <Routes>
+          <Route path="/" element={<TodosList />} />
+          <Route path="/edit/:id" element={<EditTodo />} />
+          <Route path="/create" element={<CreateTodo />} />
+        </Routes>
+      </div>
 
-          <Routes>
-            <Route path="/" exact element={<TodosList />} />
-            <Route path="/edit/:id" element={<EditTodo />} />
-            <Route path="/create" element={<CreateTodo />} />
-          </Routes>
-        </div>
-
-      </Router>
-
-    );
-  }
-}
+    </Router>
+  );
+};
 
 export default App;
