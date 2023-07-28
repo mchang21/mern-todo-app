fae7e8159f508f2ca3afbb365709a98241ef8990
Melvin Chang <melvinchang21@gmail.com>
2023-07-27 19:16:20 -0700

    added delete functionality

--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -1,17 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Trash3 } from 'react-bootstrap-icons';
 import axios from 'axios';
 
-const Todo = (props) => (
-    <tr>
-        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_description}</td>
-        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_responsible}</td>
-        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_priority}</td>
-        <td>
-            <Link to={"/edit/" + props.todo._id}>Edit</Link>
-        </td>
-    </tr>
-);
+const Todo = (props) => {
+    // Handler function for the Trash icon onClick event
+    const handleDelete = async () => {
+        try {
+            await axios.delete(`http://localhost:4000/todos/${props.todo._id}`);
+            props.onDelete(props.todo._id); // Call the onDelete function passed from the parent component
+        } catch (error) {
+            console.log("Error deleting todo: ", error);
+        }
+    };
+
+    return (
+        <tr>
+            <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_description}</td>
+            <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_responsible}</td>
+            <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_priority}</td>
+            <td>
+                <Link to={"/edit/" + props.todo._id}>Edit</Link>&nbsp;&nbsp;
+                <Trash3 
+                    className="trash"
+                    onClick={handleDelete}></Trash3>
+            </td>
+        </tr>
+    );
+};
 
 const TodosList = () => {
     const [todos, setTodos] = useState([]);
@@ -28,10 +44,14 @@ const TodosList = () => {
         
         fetchTodos();
     }, []);
+    
+    const handleDeleteTodo = (id) => {
+        setTodos((prevTodos) => prevTodos.filter(todo => todo._id !== id));
+    };
 
     const todoList = () => {
         return todos.map((currentTodo) => (
-            <Todo todo={currentTodo} key={currentTodo._id} />
+            <Todo todo={currentTodo} key={currentTodo._id} onDelete={handleDeleteTodo} />
         ));
     };
 
@@ -47,7 +67,6 @@ const TodosList = () => {
                         <th>Action</th>
                     </tr>
                 </thead>
-                <tbody>{console.log(todoList())}</tbody>
                 <tbody>{todoList()}</tbody>
             </table>
         </div>
