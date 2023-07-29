efb1f6b6a0ba2bb669319078410ac280261a7f71
Melvin Chang <melvinchang21@gmail.com>
2023-07-28 17:25:27 -0700

    moved Router here and added ModalProvider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ModalProvider from './contexts/ModalContext';
+import { BrowserRouter as Router } from "react-router-dom";
 
+import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <ModalProvider>
+        <App />
+      </ModalProvider>
+    </Router>
   </React.StrictMode>
 );
 
