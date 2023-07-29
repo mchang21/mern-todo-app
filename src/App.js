5151d033e771090a7345cc3783e4fd867a07657c
Melvin Chang <melvinchang21@gmail.com>
2023-07-28 17:28:14 -0700

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { React } from "react";
+import { Route, Link, Routes } from "react-router-dom";
 
-import CreateTodo from "./components/create-todo.component";
+import CreateTodo from "./components/create-todo.component"
 import EditTodo from "./components/edit-todo.component";
 import TodosList from "./components/todos-list.component";
 
@@ -10,40 +9,36 @@ import logo from "./logo.svg";
 
 const App = () => {
   return (
-    <Router>
-      <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" target="_blank" rel="noopener noreferrer">
-            <img src={logo} width="30" height="30" alt="Logo" />
-          </a>
-          <Link to="/" className="navbar-brand">
-            MERN-Stack Todo App
-          </Link>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav mr-auto">
-              <li className="navbar-item">
-                <Link to="/" className="nav-link">
-                  Todos
-                </Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/create" className="nav-link">
-                  Create Todo
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <br />
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" target="_blank" rel="noopener noreferrer">
+          <img src={logo} width="30" height="30" alt="Logo" />
+        </a>
 
-        <Routes>
-          <Route path="/" element={<TodosList />} />
-          <Route path="/edit/:id" element={<EditTodo />} />
-          <Route path="/create" element={<CreateTodo />} />
-        </Routes>
-      </div>
+        <h3 className="navbar-brand">
+          MERN-Stack Todo App
+        </h3>
 
-    </Router>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/" className="nav-link">
+                Todos
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+      </nav>
+      <br />
+
+      <Routes>
+        <Route path="/" element={<TodosList />} />
+        <Route path="/edit/:id" element={<EditTodo />} />
+        <Route path="/create" element={<CreateTodo />} />
+      </Routes>
+      
+    </div>
   );
 };
 
