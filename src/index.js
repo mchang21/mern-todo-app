b7214fbd3363cb3ed6cc6353c5dc3ed8a8feebe7
Melvin Chang <melvinchang21@gmail.com>
2023-07-28 19:35:21 -0700

    fixed bugs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ModalProvider from './contexts/ModalContext';
 import { BrowserRouter as Router } from "react-router-dom";
 
 import './index.css';
@@ -12,9 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <ModalProvider>
-        <App />
-      </ModalProvider>
+      <App />
     </Router>
   </React.StrictMode>
 );
