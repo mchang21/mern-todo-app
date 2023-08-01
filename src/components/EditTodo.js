47025eca5dead7f0e739e46abe8799811f05bf43
Melvin Chang <melvinchang21@gmail.com>
2023-08-01 14:53:07 -0700

    updated edit todo as a modal

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Button, Modal } from 'react-bootstrap';
 import axios from 'axios';
 
-const EditTodo = () => {
-    const navigate = useNavigate();
-    const { id } = useParams();
-
+const EditTodo = ({ id }) => {
+    // State for Todo
     const [todo, setTodo] = useState({
         todo_description: '',
         todo_responsible: '',
         todo_priority: 'Low',
         todo_completed: false
     });
+
+    // State for Modal
+    const [show, setShow] = useState(false);
+
+    // Handles for Modal
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     useEffect(() => {
         const fetchTodo = async () => {
@@ -54,99 +59,108 @@ const EditTodo = () => {
         });
     };
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
         console.log(todo);
 
-        axios.post(`http://localhost:4000/todos/update/${id}`, todo)
-            .then((res) => console.log(res.data))
-            .catch((error) => console.log(error));
-
-        navigate('/');
+        try {
+            await axios.post(`http://localhost:4000/todos/update/${id}`, todo);
+            handleClose();
+        } catch (error) {
+            console.log('Error adding todo: ', error);
+        }
     }
 
     return (
-        <div>
-            <h3 align="center">Update Todo</h3>
-            <form onSubmit={onSubmit}>
-                <div className="form-group">
-                    <label>Description: </label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        value={todo.todo_description}
-                        onChange={onChangeTodoDescription}
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Responsible: </label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        value={todo.todo_responsible}
-                        onChange={onChangeTodoResponsible}
-                    />
-                </div>
-                <div className="form-group">
-                    <div className="form-check form-check-inline">
-                        <input
-                            className="form-check-input"
-                            type="radio"
-                            name="priorityOptions"
-                            id="priorityLow"
-                            value="Low"
-                            checked={todo.todo_priority === 'Low'}
-                            onChange={onChangeTodoPriority}
-                        />
-                        <label className="form-check-label">Low</label>
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <input
-                            className="form-check-input"
-                            type="radio"
-                            name="priorityOptions"
-                            id="priorityMedium"
-                            value="Medium"
-                            checked={todo.todo_priority === 'Medium'}
-                            onChange={onChangeTodoPriority}
-                        />
-                        <label className="form-check-label">Medium</label>
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <input
-                            className="form-check-input"
-                            type="radio"
-                            name="priorityOptions"
-                            id="priorityHigh"
-                            value="High"
-                            checked={todo.todo_priority === 'High'}
-                            onChange={onChangeTodoPriority}
-                        />
-                        <label className="form-check-label">High</label>
-                    </div>
-                </div>
-                <div className="form-check">
-                    <input
-                        className="form-check-input"
-                        id="completedCheckbox"
-                        type="checkbox"
-                        name="completedCheckbox"
-                        onChange={onChangeTodoCompleted}
-                        checked={todo.todo_completed}
-                        value={todo.todo_completed}
-                    />
-                    <label className="form-check-label" htmlFor="completedCheckbox">
-                        Completed
-                    </label>
-                </div>
+        <>
+            <Button variant="link" onClick={handleShow}>
+                Edit
+            </Button>
 
-                <br />
-
-                <div className="form-group">
-                    <input type="submit" value="Update Todo" className="btn btn-primary" />
-                </div>
-            </form>
-        </div>
+            <Modal show={show} onHide={handleClose} backdrop={"static"}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Create Todo</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form onSubmit={onSubmit}>
+                        <div className="form-group">
+                            <label>Description: </label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                value={todo.todo_description}
+                                onChange={onChangeTodoDescription}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Responsible: </label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                value={todo.todo_responsible}
+                                onChange={onChangeTodoResponsible}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <div className="form-check form-check-inline">
+                                <input
+                                    className="form-check-input"
+                                    type="radio"
+                                    name="priorityOptions"
+                                    id="priorityLow"
+                                    value="Low"
+                                    checked={todo.todo_priority === 'Low'}
+                                    onChange={onChangeTodoPriority}
+                                />
+                                <label className="form-check-label">Low</label>
+                            </div>
+                            <div className="form-check form-check-inline">
+                                <input
+                                    className="form-check-input"
+                                    type="radio"
+                                    name="priorityOptions"
+                                    id="priorityMedium"
+                                    value="Medium"
+                                    checked={todo.todo_priority === 'Medium'}
+                                    onChange={onChangeTodoPriority}
+                                />
+                                <label className="form-check-label">Medium</label>
+                            </div>
+                            <div className="form-check form-check-inline">
+                                <input
+                                    className="form-check-input"
+                                    type="radio"
+                                    name="priorityOptions"
+                                    id="priorityHigh"
+                                    value="High"
+                                    checked={todo.todo_priority === 'High'}
+                                    onChange={onChangeTodoPriority}
+                                />
+                                <label className="form-check-label">High</label>
+                            </div>
+                        </div>
+                        <div className="form-check">
+                            <input
+                                className="form-check-input"
+                                id="completedCheckbox"
+                                type="checkbox"
+                                name="completedCheckbox"
+                                onChange={onChangeTodoCompleted}
+                                checked={todo.todo_completed}
+                                value={todo.todo_completed}
+                            />
+                            <label className="form-check-label" htmlFor="completedCheckbox">
+                                Completed
+                            </label>
+                        </div>
+                        <br></br>
+                        <div className="form-group">
+                            <input type="submit" value="Update Todo" className="btn btn-primary" />
+                        </div>
+                    </form>
+                </Modal.Body>
+            </Modal>
+        </>
     );
 
 };
