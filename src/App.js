00c282f8dabef94f3640d15a08d2e700e6055ece
Melvin Chang <melvinchang21@gmail.com>
2023-08-07 14:59:33 -0700

    removed/modified unused files

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
 import { React } from "react";
-import { Route, Link, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-import EditTodo from "./components/EditTodo";
 import TodosList from "./components/TodosList";
-
-import logo from "./logo.svg";
 
 const App = () => {
   return (
     <div className="container">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" target="_blank" rel="noopener noreferrer">
-          <img src={logo} width="30" height="30" alt="Logo" />
+        <a className="navbar-brand" href="/">
+          <h3 className="navbar-brand">
+            MERN-Stack Todo App
+          </h3>
         </a>
-
-        <h3 className="navbar-brand">
-          MERN-Stack Todo App
-        </h3>
-
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="navbar-item">
-              <Link to="/" className="nav-link">
-                Todos
-              </Link>
-            </li>
-          </ul>
-        </div>
-
       </nav>
+      
       <br />
-
       <Routes>
         <Route path="/" element={<TodosList />} />
-        <Route path="/edit/:id" element={<EditTodo />} />
       </Routes>
 
     </div>
